f13e78055607c1b17c9b584b630b98890af08543
isdavimoraes <isdavimoraes@gmail.com>
2023-04-10 20:41:15 -0300

    Update RESILIAcreate.sql
    Realizado ajustes nos comentários do arquivo RESILIAcreate.

--- Projeto em Grupo - BANCO DE DADOS DA RESILIA/Arquivos SQL/RESILIAcreate.sql
+++ Projeto em Grupo - BANCO DE DADOS DA RESILIA/Arquivos SQL/RESILIAcreate.sql
@@ -3,7 +3,7 @@ CREATE DATABASE RESILIA;
 
 USE RESILIA;
 
--- Feito por Lenilson #alinhado com RESILIALogic #checkDK
+-- Feito por Lenilson
 CREATE TABLE aluno(
 id_aluno INT NOT NULL PRIMARY KEY,
 nome VARCHAR (50),
@@ -12,14 +12,14 @@ email VARCHAR (50),
 endereco VARCHAR (50)
 );
 
--- Feito pela Stephanie #checkDK
+-- Feito pela Stephanie
 CREATE TABLE curso(
 id_curso INTEGER NOT NULL PRIMARY KEY,
 nome VARCHAR (100),
 turno VARCHAR (100)
 );
 
--- Feito por Davi #Feito Alterações #checkDK
+-- Feito por Davi
 CREATE TABLE aluno_curso(
 id_aluno INT NOT NULL,
 id_curso INT NOT NULL,
@@ -30,7 +30,7 @@ FOREIGN KEY (id_aluno) REFERENCES aluno(id_aluno),
 FOREIGN KEY (id_curso) REFERENCES curso(id_curso)
 );
 
--- Feito por Davi #checkDK
+-- Feito por Davi
 CREATE TABLE modulo(
 id_modulo INT NOT NULL PRIMARY KEY,
 nome VARCHAR (100),
@@ -40,7 +40,7 @@ proj_ind2 VARCHAR(5),
 proj_grup VARCHAR(5)
 );
 
--- Feito por Davi #checkDK
+-- Feito por Davi
 CREATE TABLE curso_modulo(
 id_curso INT NOT NULL,
 id_modulo INT NOT NULL,
@@ -49,14 +49,14 @@ FOREIGN KEY (id_curso) REFERENCES curso(id_curso),
 FOREIGN KEY (id_modulo) REFERENCES modulo(id_modulo)
 );
 
---Feito por Douglas #Foi feito alterações #checkDK
+--Feito por Douglas
 CREATE TABLE departamento(
 id_departamento INTEGER NOT NULL PRIMARY KEY,
 nome VARCHAR (50),
 setor VARCHAR (50)
 );
 
--- Feito por Matheus #Foi feito alterações #checkDK
+-- Feito por Matheus
 CREATE TABLE facilitador(
 id_facilitador INT NOT NULL PRIMARY KEY,
 nome VARCHAR (50),
@@ -67,7 +67,7 @@ id_departamento INTEGER NOT NULL,
 FOREIGN KEY (id_departamento) REFERENCES departamento(id_departamento)
 );
 
--- Feito por Davi #checkDK
+-- Feito por Davi
 CREATE TABLE modulo_facilitador(
 id_modulo INT NOT NULL,
 id_facilitador INT NOT NULL,
