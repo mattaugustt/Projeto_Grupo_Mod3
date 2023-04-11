6fb25db476afb14156801c10298bd6279dc641c8
Douglas Klem <120439075+KlemDoug@users.noreply.github.com>
2023-04-11 03:46:45 -0300

    Revisão
    Cabeçalho e Alinhamento pro README.

--- Projeto em Grupo - BANCO DE DADOS DA RESILIA/Arquivos SQL/RESILIACreate.sql
+++ Projeto em Grupo - BANCO DE DADOS DA RESILIA/Arquivos SQL/RESILIACreate.sql
@@ -1,9 +1,24 @@
---Criação da database
+------------------------------------------------------------------
+--  SENAC/RESILIA - Formação em Análise de Dados (FAD)          --
+--  Projeto em Grupo - Módulo 3 - Banco de Dados da Resilia     --
+--  SQL Server Management Studio 19 -*- coding: utf-8 -*-       --
+--  Criado por: Davi de Moraes Novaes, Douglas Klem Portugal do --
+--  Amaral, Matheus Augusto de Souza, Lenilson dos Reis Barros  --
+--  e Stephanie Ferreira Vale                                   --
+--  Data de criação: 10/04/2023                                 --
+--  versão = 6.19(64-bit)                                       --
+------------------------------------------------------------------
+--
+--BANCO DE DADOS DA RESILIA--
+--
+--criação das tabelas para cada entidade--
+--
+--Criação da Database--
 CREATE DATABASE RESILIA;
-
+--
 USE RESILIA;
-
--- Feito por Lenilson
+--
+--Entidade 1 - Feito pelo Lenilson--
 CREATE TABLE aluno(
 id_aluno INT NOT NULL PRIMARY KEY,
 nome VARCHAR (50),
@@ -11,15 +26,15 @@ telefone INT,
 email VARCHAR (50),
 endereco VARCHAR (50)
 );
-
--- Feito pela Stephanie
+--
+--Entidade 2 - Feito pela Stephanie--
 CREATE TABLE curso(
 id_curso INTEGER NOT NULL PRIMARY KEY,
 nome VARCHAR (100),
 turno VARCHAR (100)
 );
-
--- Feito por Davi
+--
+--Entidade 3 - Feito pelo Davi--
 CREATE TABLE aluno_curso(
 id_aluno INT NOT NULL,
 id_curso INT NOT NULL,
@@ -29,8 +44,8 @@ PRIMARY KEY (id_aluno,id_curso),
 FOREIGN KEY (id_aluno) REFERENCES aluno(id_aluno),
 FOREIGN KEY (id_curso) REFERENCES curso(id_curso)
 );
-
--- Feito por Davi
+--
+--Entidade 4 - Feito pelo Davi--
 CREATE TABLE modulo(
 id_modulo INT NOT NULL PRIMARY KEY,
 nome VARCHAR (100),
@@ -39,8 +54,8 @@ proj_ind1 VARCHAR(5),
 proj_ind2 VARCHAR(5),
 proj_grup VARCHAR(5)
 );
-
--- Feito por Davi
+--
+--Entidade 5 - Feito pelo Davi--
 CREATE TABLE curso_modulo(
 id_curso INT NOT NULL,
 id_modulo INT NOT NULL,
@@ -48,15 +63,15 @@ PRIMARY KEY (id_curso,id_modulo),
 FOREIGN KEY (id_curso) REFERENCES curso(id_curso),
 FOREIGN KEY (id_modulo) REFERENCES modulo(id_modulo)
 );
-
---Feito por Douglas
+--
+--Entidade 6 - Feito pelo Douglas--
 CREATE TABLE departamento(
 id_departamento INTEGER NOT NULL PRIMARY KEY,
 nome VARCHAR (50),
 setor VARCHAR (50)
 );
-
--- Feito por Matheus
+--
+--Entidade 7 - Feito pelo Matheus--
 CREATE TABLE facilitador(
 id_facilitador INT NOT NULL PRIMARY KEY,
 nome VARCHAR (50),
@@ -66,8 +81,8 @@ endereco VARCHAR (50),
 id_departamento INTEGER NOT NULL,
 FOREIGN KEY (id_departamento) REFERENCES departamento(id_departamento)
 );
-
--- Feito por Davi
+--
+--Entidade 8 - Feito pelo Davi--
 CREATE TABLE modulo_facilitador(
 id_modulo INT NOT NULL,
 id_facilitador INT NOT NULL,
@@ -75,3 +90,5 @@ PRIMARY KEY (id_modulo,id_facilitador),
 FOREIGN KEY (id_modulo) REFERENCES modulo(id_modulo),
 FOREIGN KEY (id_facilitador) REFERENCES facilitador(id_facilitador)
 );
+--finalização das tabelas para cada entidade--
+------------------------------------------------------------------
